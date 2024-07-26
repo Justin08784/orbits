0d077b410b4a6ad1254a848160fad6e5a8af8673
Eunsoo Justin Shin <shinej@uchicago.edu>
2024-07-27 03:27:43 +0900

    added (yet) unused constants for sympletic integrators

--- Assets/Scripts/Sphere.cs
+++ Assets/Scripts/Sphere.cs
@@ -6,21 +6,40 @@ using UnityEngine;
 using UnityEngine.Animations;
 using UnityEngine.Assertions;
 
+
+
+
 public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
 
     private double v = 0;
+    private double x = 1;
     private double a = 0;
 
-    private double delta_t = 0.01;
+    private double dt = 0.01;
 
-    private double k_by_m = 5;
+    private double k_by_m = 1;
 
     private int upd_cnt = 0;
+
+    private readonly double c = Math.Pow(2.0, 1.0/3.0);
+
+    double[] cs;
+    double[] ds;
     void Start()
     {
-        transform.position = new Vector3(1, 0, 0);
+        transform.position = new Vector3((float) x, 0, 0);
+        a = -k_by_m * x;
+
+        cs = new double[4] {0.5, 0.5*(1.0-c), 0.5*(1.0-c), 0.5};
+        ds = new double[4] {0.0,         1.0,          -c, 1.0};
+
+        double divisor = 2.0 - c;
+        for (int i = 0; i < cs.GetLength(0); i++) {
+            cs[i] /= divisor;
+            ds[i] /= divisor;
+        }
     }
 
     // Update is called once per frame
@@ -31,9 +50,10 @@ public class NewBehaviourScript : MonoBehaviour
         } else if (upd_cnt == 1) {
             Debug.Log(string.Format("P1 a = {0}, v = {1}, x = {2}", a, v, transform.position.x));
         }
-        transform.position = new Vector3(transform.position.x + (float) (v * delta_t), transform.position.y, transform.position.z);
-        v += a * delta_t;
-        a = -k_by_m * transform.position.x;
+        x += (float) (v * dt);
+        transform.position = new Vector3((float) x, transform.position.y, transform.position.z);
+        v += a * dt;
+        a = -k_by_m * x;
         upd_cnt += 1;
     }
 }
