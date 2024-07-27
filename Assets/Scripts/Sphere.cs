29edd617cea95fad0219e0f92c6966f764333b97
Eunsoo Justin Shin <shinej@uchicago.edu>
2024-07-27 18:22:38 +0900

    removing unused imports

--- Assets/Scripts/Sphere.cs
+++ Assets/Scripts/Sphere.cs
@@ -1,10 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Animations;
-using UnityEngine.Assertions;
 
 
 
